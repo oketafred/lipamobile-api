97eb13dd66d542d82d2e70d166a909dd52d0772f
Oketa Fred <oketafred@gmail.com>
2020-10-06 13:15:28 +0300

    Added Get Transactionstatus and getAllTransactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Wallet;
+use App\Models\Transaction;
 
 class TransactionController extends Controller
 {
-    //
+    public function getTransactionStatus($referenceNumber)
+    {
+        $transaction = Transaction::where('reference_number', $referenceNumber)->first();
+        if (!$transaction) {
+            return response()->json([
+                'errors' => 'No Transaction found with the provided Reference Number',
+            ]);
+        }
+
+        return response()->json([
+            'message' => $transaction,
+        ]);
+    }
+
+    public function getAllTransactions($phoneNumber)
+    {
+        $wallet       = Wallet::where('phone_number', $phoneNumber)->first();
+        if (!$wallet) {
+            return response()->json([
+                'errors' => 'Transaction records found with the provided Phone Number',
+            ]);
+        }
+        $transactions = Transaction::where('walletNo', $wallet->walletNo)->get();
+        return response()->json([
+            'message' => $transactions,
+        ]);
+    }
 }
